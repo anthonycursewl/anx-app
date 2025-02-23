7f1a3d399ff3636960d18db41d1471dc67dd1c7c
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-02-23 18:07:07 -0400

    [change] Point to the API in production.

--- src/config/anx.config.breadriuss.ts
+++ src/config/anx.config.breadriuss.ts
@@ -5,7 +5,7 @@ const config = {
     api_version: 'v1',
     entity: 'anx',
     type: 'api',
-    url: 'http://localhost:3000'
+    url: 'https://anx-app-backend-production.up.railway.app'
 }
 
 export const API_URL = `${config.url}/${config.type}/${config.entity}/${config.api_version}`;
