129b6cf3c5bf07743afddc806cbf79d1c3446680
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-04-13 20:55:39 -0400

    *

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,61 @@
 // services
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import UserPosts from './infrastructure/ui/components/main/Posts/user-posts'
 import { Navigate } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
+import HandleLoading from './shared/components/Loaders/HandleLoading'
+
 
 // Components
-import MainP from './infrastructure/ui/components/main/main-p'
-import LoginComponent from './infrastructure/ui/components/auth/login'
-import ProtectedRoutes from './shared/components/ProtectedRoutes'
-import Profile from './infrastructure/ui/components/profile/profile'
-import StatusPost from './infrastructure/ui/components/main/Posts/components/StatusPost/StatusPost'
-import Portfolio from './infrastructure/ui/components/portfolios/portfolio'
-import NotificationPop from './shared/components/Notifications/NotificationPop'
-
+const MainP = lazy(() => import('./infrastructure/ui/main/main-p'))
+const LoginComponent =  lazy(() => import('./infrastructure/ui/auth/login'))
+const UserPosts = lazy(() => import('./infrastructure/ui/main/Posts/user-posts'))
+const ProtectedRoutes = lazy(() => import('./shared/components/ProtectedRoutes'))
+const Profile = lazy(() => import('./infrastructure/ui/profile/profile')) 
+const StatusPost = lazy(() => import('./infrastructure/ui/main/Posts/components/StatusPost/StatusPost'))
+const Portfolio = lazy(() => import ('./infrastructure/ui/portfolios/portfolio'))
+const NotificationPop = lazy(() => import ('./shared/components/Notifications/NotificationPop')) 
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
+      <Suspense fallback={HandleLoading()}>
+        <Routes>
+            <Route path="/feed" element={
+              <ProtectedRoutes>
+                <MainP />
+              </ProtectedRoutes>
+              }>
+              <Route path="" element={<UserPosts /> } />
+            </Route>
+            <Route path='/profile/:username' element={
+              <ProtectedRoutes>
+                <MainP />
+              </ProtectedRoutes>
+            }>
 
-    
-        <Route path="/feed" element={
-          <ProtectedRoutes>
-            <MainP />
-          </ProtectedRoutes>
-          }>
-          <Route path="" element={<UserPosts /> } />
-        </Route>
-        <Route path='/profile/:username' element={
-          <ProtectedRoutes>
-            <MainP />
-          </ProtectedRoutes>
-        }>
+              <Route path='' element={<Profile />}/>
+            </Route>
 
-          <Route path='' element={<Profile />}/>
-        </Route>
+            <Route path='/post/status/:id_post' element={ 
+              <ProtectedRoutes>
+                  <MainP />
+                </ProtectedRoutes>
+            }>
 
-        <Route path='/post/status/:id_post' element={ 
-          <ProtectedRoutes>
-              <MainP />
-            </ProtectedRoutes>
-        }>
+                <Route path='info' element={<StatusPost />}/>
+            </Route>
 
-            <Route path='info' element={<StatusPost />}/>
-        </Route>
+            <Route path='/about/anthony' element={<Portfolio />}/>
 
-        <Route path='/about/anthony' element={<Portfolio />}/>
-
-        <Route path='*' element={<h1>404</h1>} />
-        <Route path='/login' element={<LoginComponent />}/>
-        <Route path='/' element={<Navigate to={"/feed"}/>}/>
-      </Routes>
-
-      <NotificationPop />
+            <Route path='*' element={<h1>404</h1>} />
+            <Route path='/login' element={<LoginComponent />}/>
+            <Route path='/' element={<Navigate to={"/feed"}/>}/>
+          </Routes>
+        </Suspense>
+        
+      <Suspense fallback={HandleLoading()}>
+        <NotificationPop />
+      </Suspense>
     </BrowserRouter>
   )
 }
