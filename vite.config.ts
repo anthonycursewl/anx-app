aabc705ba8fa19afdb82c89d9131b9951fcc8262
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-04-14 11:04:48 -0400

    revert commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     }
   }
+=======
+>>>>>>> parent of 286625a ([add] relatice routes --pending)
 })
