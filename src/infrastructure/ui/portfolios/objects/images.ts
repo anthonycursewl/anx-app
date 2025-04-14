eb50ba30a83d9578de43e42cf26bef128a1fe6d4
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-04-13 21:11:49 -0400

    * --fix

--- src/infrastructure/ui/portfolios/objects/images.ts
+++
@@ -1,5 +0,0 @@
-export const bitter = ['/projects/bitter-1.webp', '/projects/bitter-2.webp', '/projects/bitter-3.webp',
-    '/projects/bitter-4.webp', '/projects/bitter-5.webp', '/projects/bitter-6.webp', '/projects/bitter-7.webp'
-]
-
-export const forcegym = ['/projects/forcegym-1.webp', '/projects/forcegym-2.webp', '/projects/forcegym-3.webp', '/projects/forcegym-4.webp']
